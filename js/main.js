507d43a39dd4158880104bd0bbe547df214fcd8e
Oksana <oksana.shpack@gmail.com>
2021-06-09 20:51:24 +1000

    Update More Detail homework

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@ const AVATAR_RANGE = {
   MIN: 1,
   MAX: 8,
 };
-const OFFER_TITLE = "Wonderful property in Tokyo";
-const OFFER_TYPE = ["palace", "flat", "house", "bungalow", "hotel"];
-const OFFER_CHECKSET = ["12:00", "13:00", "14:00"];
+const OFFER_TITLE = 'Wonderful property in Tokyo';
+const OFFER_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const OFFER_CHECKSET = ['12:00', '13:00', '14:00'];
 const OFFER_FEATURES = [
-  "wifi",
-  "dishwasher",
-  "parking",
-  "washer",
-  "elevator",
-  "conditioner",
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
 ];
 const OFFER_DESCRIPTION =
-  "Comfortable real estate in Tokyo. A cozy option for the weary traveler.";
+  'Comfortable real estate in Tokyo. A cozy option for the weary traveler.';
 const OFFER_PHOTOS = [
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg",
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 const LOCATION = {
   LAT: {
@@ -50,6 +50,16 @@ function getRandomPositiveInteger(a, b) {
 const getRandomArrayElement = (elements) =>
   elements[getRandomPositiveInteger(0, elements.length - 1)];
 
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
 const createAd = () => {
   const location = {
     lat: getRandomPositiveFloat(LOCATION.LAT.MIN, LOCATION.LAT.MAX, 5),
@@ -57,10 +67,7 @@ const createAd = () => {
   };
   return {
     author: {
-      avatar: `img/avatars/user0${getRandomPositiveInteger(
-        AVATAR_RANGE.MIN,
-        AVATAR_RANGE.MAX
-      )}.png`,
+      avatar: `img/avatars/user0${getRandomPositiveInteger(AVATAR_RANGE.MIN, AVATAR_RANGE.MAX)}.png`,
     },
     offer: {
       title: OFFER_TITLE,
@@ -69,22 +76,17 @@ const createAd = () => {
       type: getRandomArrayElement(OFFER_TYPE),
       rooms: getRandomPositiveInteger(1, 5),
       guests: getRandomPositiveInteger(1, 10),
-      checkin: getRandomArrayElement(OFFER_CHECKSET),
+      checkin: getRandomArrayElement(shuffleArray(OFFER_CHECKSET)),
       checkout: getRandomArrayElement(OFFER_CHECKSET),
-      features: new Array(
-        getRandomPositiveInteger(1, OFFER_FEATURES.length - 1)
-      )
-        .fill(null)
-        .map((element, index) => OFFER_FEATURES[index]),
+      features: shuffleArray(OFFER_FEATURES).slice(0, getRandomPositiveInteger(1, OFFER_FEATURES.length - 1)),
       description: OFFER_DESCRIPTION,
-      photos: OFFER_PHOTOS.slice(
-        0,
-        getRandomPositiveInteger(1, OFFER_PHOTOS.length - 1)
-      ),
+      photos: OFFER_PHOTOS.slice(0, getRandomPositiveInteger(1, OFFER_PHOTOS.length - 1)),
     },
     location,
   };
 };
 
 const similarAds = new Array(SIMILAR_AD_COUNT).fill(null).map(() => createAd());
+
 console.log(similarAds);
+// similarAds();
