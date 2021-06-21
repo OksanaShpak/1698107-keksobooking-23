fc04a4b6fea36848701f6ddfff5d4264620b2cc4
Oksana <oksana.shpack@gmail.com>
2021-06-21 22:35:50 +1000

    fix popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,10 @@
 import { createAds } from './data.js';
+import { declOfNum } from './utils/declension-of-numbers.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
-const similarAdTemplate = document.querySelector('#card').content.querySelector('.popup');
+const similarAdTemplate = document
+  .querySelector('#card')
+  .content.querySelector('.popup');
 
 const types = {
   flat: 'Квартира',
@@ -12,28 +15,98 @@ const types = {
 };
 
 const similarAds = createAds();
-const updateElement = (element, content, property='innerHTML')=>{
-  if (content.length === 0 ){
-    element.classList.add('hidden');
-    return false;
-  }
-  element[property] = content;
-  return true;
-};
 
-similarAds.forEach(({offer, author}  ) => {
+const similarAdFragment = document.createDocumentFragment();
+
+similarAds.forEach(({ offer, author }) => {
   const adElement = similarAdTemplate.cloneNode(true);
-  updateElement(adElement.querySelector('.popup__title'), offer.title, 'textContent');
-  // adElement.querySelector('.popup__title').textContent = offer.title;
-  adElement.querySelector('.popup__text--address').textContent = offer.address;
-  adElement.querySelector('.popup__text--price').innerHTML = `${offer.price} <span>₽/ночь</span>`;
-  adElement.querySelector('.popup__type').textContent = types[offer.type];
-  adElement.querySelector('.popup__text--capacity').innerHTML = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  adElement.querySelector('.popup__features').innerHTML = offer.features.map((feature) =>`<li class="popup__feature popup__feature--${feature}"></li>`).join('');
-  adElement.querySelector('.popup__description').textContent = offer.description;
-  adElement.querySelector('.popup__photos').innerHTML=offer.photos.map((src)=>`<img src="${src}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`).join('');
-  adElement.querySelector('.popup__avatar').src = author.avatar;
 
-  mapCanvas.appendChild(adElement);
+  const popupTitle = adElement.querySelector('.popup__title');
+  if (offer.title) {
+    popupTitle.textContent = offer.title;
+  } else {
+    popupTitle.remove();
+  }
+
+  const popupAddress = adElement.querySelector('.popup__text--address');
+  if (offer.address) {
+    popupAddress.textContent = offer.address;
+  } else {
+    popupAddress.remove();
+  }
+
+  const popupPrice = adElement.querySelector('.popup__text--price');
+  if (offer.price) {
+    popupPrice.textContent = `${offer.price} ₽/ночь`;
+  } else {
+    popupPrice.remove();
+  }
+
+  const popupType = adElement.querySelector('.popup__type');
+  if (types[offer.type]) {
+    popupType.textContent = types[offer.type];
+  } else {
+    popupType.remove();
+  }
+
+  const popupCapacity = adElement.querySelector('.popup__text--capacity');
+  if (offer.rooms && offer.guests) {
+    popupCapacity.textContent = `${offer.rooms} ${declOfNum(offer.rooms, [
+      'комната',
+      'комнаты',
+      'комнат',
+    ])} для ${offer.guests} ${declOfNum(offer.guests, [
+      'гость',
+      'гостей',
+      'гостей',
+    ])}`;
+  } else {
+    popupCapacity.remove();
+  }
+
+  const popupFeatures = adElement.querySelector('.popup__features');
+  if (offer.features){
+    const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
+    popupFeatures.querySelectorAll('.popup__feature').forEach((item) => {
+      const modifier = item.classList[1];
+      if (!modifiers.includes(modifier)) {
+        item.remove();
+      }
+    });
+  } else {
+    popupFeatures.remove();
+  }
+
+  const popupDescription = adElement.querySelector('.popup__description');
+  if (offer.description) {
+    popupDescription.textContent = offer.description;
+  } else {
+    popupDescription.remove();
+  }
+
+  const popupPhotos = adElement.querySelector('.popup__photos');
+  if (offer.photos) {
+    const photoTemplate = popupPhotos.querySelector('.popup__photo');
+    const fragmentPhoto = document.createDocumentFragment();
+    offer.photos.map((photo) => {
+      const newPhoto = photoTemplate.cloneNode(true);
+      newPhoto.src = photo;
+      fragmentPhoto.appendChild(newPhoto);
+    });
+    popupPhotos.innerHTML = '';
+    popupPhotos.appendChild(fragmentPhoto);
+  } else {
+    popupPhotos.remove();
+  }
+
+  const popupAvatar = adElement.querySelector('.popup__avatar');
+  if (author.avatar) {
+    popupAvatar.src = author.avatar;
+  } else {
+    popupAvatar.remove();
+  }
+
+  similarAdFragment.appendChild(adElement);
 });
 
+mapCanvas.appendChild(similarAdFragment);
